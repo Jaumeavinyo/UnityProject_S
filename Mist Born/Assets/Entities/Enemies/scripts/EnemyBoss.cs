062a5204e9362135d10cae6ef983c7459cf10d7f
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-08-04 21:39:41 +0200

    bosss chases and attacks

--- Mist Born/Assets/Entities/Enemies/scripts/EnemyBoss.cs
+++ Mist Born/Assets/Entities/Enemies/scripts/EnemyBoss.cs
@@ -18,8 +18,19 @@ public class EnemyBoss : MonoBehaviour
     public FSM_CharMov player;
 
     public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    public Rigidbody2D rb;
 
     Vector3 spawnPos;
+
+    float moveDir;
+    bool bAttackNow;
+    public float runAnimationSpeedCorrectionLegPull;
+    public float runAnimationSpeedCorrectionLegLand;
+    public float spotDistance;
+    public float meleeAttackDistance;
+    public float chaseSpeed;
+
 
     void Start()
     {
@@ -29,12 +40,14 @@ public class EnemyBoss : MonoBehaviour
         currBossState = BossState.WANDERING;
 
         spawnPos = gameObject.transform.position;
+
+        bAttackNow = false;
     }
 
     void Update()
     {
         SetSpriteDirection();//should not be called if in the middle of a attack or action
-        
+        SetCurrentAnim();
         
     }
 
@@ -43,20 +56,43 @@ public class EnemyBoss : MonoBehaviour
         switch (currBossState)
         {
             case BossState.WANDERING:
-                {                                     
-
+                {
+                    checkBossState();
                     break;
                 }
             case BossState.CHASE:
                 {
+                    //total num of frames: 11
+                    //leg pulls from: 8- 11
+                    //leg lands in 6-7
+                    float speedmultiplyer = 1.0f;
+                    float runAnimTime = animator.GetCurrentAnimatorStateInfo(0).normalizedTime % 1;
+                    //when leg pulling
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_Run") && runAnimTime > 0.72f)//8/100*8(the frame)
+                    {
+                        speedmultiplyer = runAnimationSpeedCorrectionLegPull;
 
+                        //when leg in air or ground
+                    }else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_Run") && runAnimTime < 0.72f){
+                        speedmultiplyer = runAnimationSpeedCorrectionLegLand;
+                    }
+                    Vector2 velDir = rb.linearVelocity;
+                    velDir.x = chaseSpeed * moveDir*speedmultiplyer;
+                    rb.linearVelocity = velDir;
 
+                    checkBossState();
 
                     break;
                 }
             case BossState.ATTACK:
                 {
-
+                    if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_LegSlam"))
+                    {
+                        bAttackNow = true;
+                    }else if(animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_LegSlam") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
+                    {
+                        checkBossState();
+                    }
 
 
                     break;
@@ -64,16 +100,93 @@ public class EnemyBoss : MonoBehaviour
         }
     }
 
+    //CHECK FOR STATE CHANGES
+    void checkBossState()
+    {
+        switch (currBossState)
+        {
+            case BossState.WANDERING:
+                {
+                    if(Mathf.Abs(player.transform.position.x - this.transform.position.x) < spotDistance)
+                    {
+                        currBossState = BossState.CHASE;
+                    }
+                    break;
+                }
+            case BossState.CHASE:
+                {
+
+                    if (Mathf.Abs(player.transform.position.x - this.transform.position.x) < meleeAttackDistance)
+                    {
+                        currBossState = BossState.ATTACK;
+                        Vector2 velDir = rb.linearVelocity;
+                        velDir.x = 0.0f;
+                        rb.linearVelocity = velDir;
+                    }
+
+                    break;
+                }
+            case BossState.ATTACK:
+                {
+                    if(Mathf.Abs(player.transform.position.x - this.transform.position.x) > meleeAttackDistance)
+                    {
+                        currBossState = BossState.CHASE;
+                    }
+
+
+                    break;
+                }
+        }
+    }
+
+    //CHECK FOR ANIM STATES
+    void SetCurrentAnim()
+    {
+        switch (currBossState)
+        {
+            case BossState.WANDERING:
+                {
+                    if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_Idle"))
+                    {
+                        animator.Play("Boss_Idle");
+                    }
+                    break;
+                }
+            case BossState.CHASE:
+                {
+                   
+                    if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Boss_Run"))
+                    {
+                        animator.Play("Boss_Run");
+                    }
+
+                    break;
+                }
+            case BossState.ATTACK:
+                {
+                    if (bAttackNow)
+                    {
+                        animator.Play("Boss_LegSlam");                       
+                        bAttackNow = false;
+                    }
+
+
+                    break;
+                }
+        }
+    }
 
     void SetSpriteDirection()
-    {     
+    {
         if ((this.transform.position.x - player.transform.position.x) > 0.0)
         {
             gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            moveDir = -1;
         }
         else
         {
             gameObject.transform.localScale = new Vector3(1, 1, 1);
+            moveDir = 1;
         }
     }
 }
